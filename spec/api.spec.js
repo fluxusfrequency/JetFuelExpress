9ca2a59b750a482c67e101c56a9f74bc814511a0
Ben Lewis <bennlewis@gmail.com>
2013-11-21 15:56:36 -0700

    created link shortening logic and links

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -6,8 +6,7 @@ describe("api route", function() {
     request("http://localhost:3000/api/urls", function(error, response, body) {
       expect(response.statusCode).toBe(200);
       expect(response.body).toContain('[{');
-      expect(response.body).toContain('{"title":');
-      expect(response.body).toContain(',"shortenedUrl":');
+      expect(response.body).toContain('"shortenedUrl":');
       expect(response.body).toContain(',"originalUrl":');
       expect(response.body).toContain(',"createdDate":');
       expect(response.body).toContain('}]');
@@ -37,18 +36,16 @@ describe("api route", function() {
   });
 
   it("should respond with json to url GET json request", function(done) {
-    // var req_path = "";
-    // var new_id = "";
-    // var req = request.post("http://localhost:3000/api/urls", {form:{"title": "howdy", "shortenedUrl": "bit.ly/23k49c", "originalUrl": "www.google.com", "createdDate": "2rdkwe"}}, 
+    // request.post("http://localhost:3000/api/urls", {form:{"originalUrl": "www.google.com"}}, 
     //   function(error, response, body) {
-    //     // var new_id = JSON.parse(response.body)._id;
-    //     req_path += "http://localhost:3000/api/" + new_id.toString(); 
-    //     new_id += JSON.parse(response.body)._id.toString();
-    //   });
-    request.get( "http://localhost:3000/api/urls/WDtZVm", function(error, response, body) {
+    //     var shortened = JSON.parse(body).shortenedUrl.toString();
+    //     console.log(shortened);
+    // });
+    // console.log(shortened);
+    request.get( "http://localhost:3000/api/urls/EF0xlx", function(error, response, body) {
         expect(response.statusCode).toBe(200);
         expect(response.body).toContain('{');
-        expect(response.body).toContain('"shortenedUrl":"WDtZVm"');
+        expect(response.body).toContain('"shortenedUrl":"EF0xlx"');
         expect(response.body).toContain(',"originalUrl":"www.geocities.com"');
         expect(response.body).toContain(',"createdDate":');
         expect(response.body).toContain('}');
@@ -61,12 +58,12 @@ describe("api route", function() {
 
   it("should respond with json to url UPDATE json request", function(done) {
     
-    request.put("http://localhost:3000/api/urls/WDtZVm", {form:{"title": "howdy_doody", "shortenedUrl": "WDtZVm", "originalUrl": "www.geocities.com"}},
+    request.put("http://localhost:3000/api/urls/EF0xlx", {form:{"title": "howdy_doody", "shortenedUrl": "EF0xlx", "originalUrl": "www.geocities.com"}},
       function(error, response, body) {
         expect(response.statusCode).toBe(200);
         expect(response.body).toContain('{');
         expect(response.body).toContain('"title":"howdy_doody"');
-        expect(response.body).toContain('"shortenedUrl":"WDtZVm"');
+        expect(response.body).toContain('"shortenedUrl":"EF0xlx"');
         expect(response.body).toContain(',"originalUrl":"www.geocities.com"');
         expect(response.body).toContain(',"createdDate":');
         expect(response.body).toContain('}');
@@ -79,7 +76,7 @@ describe("api route", function() {
 
   xit("should respond with json to url DELETE json request", function(done) {
 
-    request.del("http://localhost:3000/api/urls/WDtZVm", function(error, response, body) {
+    request.del("http://localhost:3000/api/urls/EF0xlx", function(error, response, body) {
       expect(response.body).toContain("Success")
       expect(error).toBeNull;
       done();
