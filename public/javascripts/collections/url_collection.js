77c10b7e49c02305a445027c33f5b643c04acb49
Ben Lewis <bennlewis@gmail.com>
2013-11-21 16:22:26 -0700

    setup collection comparator

--- public/javascripts/collections/url_collection.js
+++ public/javascripts/collections/url_collection.js
@@ -2,5 +2,20 @@ var jetfuelexpress = jetfuelexpress || {};
 
 jetfuelexpress.UrlCollection = Backbone.Collection.extend({
   model: jetfuelexpress.UrlModel,
-  url: '/api/urls'
+  url: '/api/urls',
+
+  initialize: function() {
+    this.sort_key = "createdDate";
+    this.sort();
+  },
+
+  comparator: function(a, b) {
+    a = a.get(this.sort_key);
+    b = b.get(this.sort_key);
+    return a > b ? -1
+        :  a < b ?  1
+        :           0;
+  }
+
+
 });
