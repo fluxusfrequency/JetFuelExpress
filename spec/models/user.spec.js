d76c0a5476138b8f3bcac39690083998cec408d4
Ben Lewis <bennlewis@gmail.com>
2013-11-25 15:19:04 -0700

    generated user model with specs

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -10,11 +10,32 @@ describe("UserModel", function() {
 
   describe("User Attributes", function() {
 
+    it("should be invalid without attrs", function() {
+      var user = new User();
+      expect(user.save).toThrow();
+    });
+
     it("should have a username attribute", function() {
       var user = new User({ 'username': 'admin'});
       expect(user.username).toBe('admin');
     });
 
+    it("should have an email attribute", function() {
+      var user = new User({ 'username': 'admin', 'email': 'admin@example.com'});
+      expect(user.email).toBe('admin@example.com');
+    });
+
+    it("should have a password attribute", function() {
+      var user = new User({ 'username': 'admin', 'email': 'admin@example.com', 'password': 'password'});
+      expect(user.password).toBe('password');
+    });
+
+    it("should have a createdDate attribute", function() {
+      var user = new User({ 'username': 'admin', 'email': 'admin@example.com',
+        'password': 'password', 'createdDate': Date.now()});
+      expect(user.createdDate).not.toBe(undefined);
+      expect(user.createdDate.constructor.name).toBe('Date');
+    });
 
   });
 
