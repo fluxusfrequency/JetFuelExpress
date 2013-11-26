c86b06b7ba6c7028b36adbcd3547d920c614c751
Ben Lewis <bennlewis@gmail.com>
2013-11-26 13:11:12 -0700

    added production env uri codes to server.js

--- server.js
+++ server.js
@@ -54,6 +54,21 @@ app.configure( function() {
 
 });
 
+var uristring = 
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/jetfuelexpress';
+
+if('production' == app.get('env')) {
+  mongoose.connect(uristring, function (err, res) {
+    if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+    console.log ('Succeeded connected to: ' + uristring);
+    }
+  });
+}
+
 // Development only
 if ('development' == app.get('env')) {
   app.use( express.errorHandler({ dumpExceptions: true, showStack: true}));
